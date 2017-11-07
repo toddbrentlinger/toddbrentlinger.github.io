706207f3d1b0dfd9608fba219eb29de2f535ab27
Todd Brentlinger <toddbrentlinger@users.noreply.github.com>
2017-11-06 16:15:54 -0800

    Add files to update website

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+﻿var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === '../images/todd-brentlinger-profile-pic.jpg') {
+        myImage.setAttribute('src', '../images/james_bond_backdrop_opinion.jpg');
+        myImage.setAttribute('width', '400');
+        myImage.setAttribute('height', '250');
+    } else {
+        myImage.setAttribute('src', '../images/todd-brentlinger-profile-pic.jpg');
+        myImage.setAttribute('width', '250');
+        myImage.setAttribute('height', '250');
+    }
+}
