ad9fb7192a9aa58bd3dc5fa7763441fa9de2c518
Todd Brentlinger <toddbrentlinger@users.noreply.github.com>
2018-02-06 19:07:33 -0800

    Add updated files from Visual Studio project

---
+++ webdev/javascipt/bouncing-balls/main.js
@@ -0,0 +1,100 @@
+﻿// setup canvas
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min, max) {
+    var num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+}
+
+// Ball constructor
+function Ball(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+}
+
+// Draw method on Ball prototype
+Ball.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+// Update method on Ball prototype
+Ball.prototype.update = function () {
+    if ((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+}
+
+// Collision detect method on Ball prototype
+Ball.prototype.collisionDetect = function () {
+    for (var i = 0; i < balls.length; i++) {
+        if (!(this === balls[i])) {
+            var dx = this.x - balls[i].x;
+            var dy = this.y - balls[i].y;
+            var distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[i].size) {
+                balls[i].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+            }
+        }
+    }
+}
+
+// Array to store Ball objects
+var balls = [];
+
+// Function: Animation loop
+function loop() {
+    ctx.fillStyle = 'rgba(0,0,0,0.4)';
+    ctx.fillRect(0, 0, width, height);
+
+    while (balls.length < 25) {
+        var ball = new Ball(
+            random(0, width),
+            random(0, height),
+            random(-7, 7),
+            random(-7, 7),
+            'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+            random(10, 20)
+            );
+        balls.push(ball);
+    }
+
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
+    }
+
+    requestAnimationFrame(loop);
+}
+// Call animation loop to get animation started
+loop();
---
+++ webdev/javascipt/image-gallery/main.js
@@ -0,0 +1,38 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+var btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+for (let i = 1; i <= 5; i++) {
+    var newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    thumbBar.appendChild(newImage);
+
+    // newImage onclick event handler
+    newImage.onclick = function (event) {
+        // reference to src attribute of image clicked on
+        var imageSource = event.target.getAttribute('src');
+        // set displayedImage to image clicked on
+        setDisplayedImage(imageSource);
+    };
+};
+
+function setDisplayedImage(source) {
+    displayedImage.setAttribute('src', source);
+}
+
+/* Wiring up the Darken/Lighten button */
+btn.onclick = function () {
+    if (this.getAttribute('class') === 'dark') {
+        this.setAttribute('class', 'light');
+        this.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        this.setAttribute('class', 'dark');
+        this.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+};
+
